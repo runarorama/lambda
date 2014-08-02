8fa9be01fa5d8a0ff04fae87656224536a9e2f89
Greg Hale <ImAlsoGreg@gmail.com>
2014-08-02 14:36:47 -0400

    Added some tests

--- haskell/src/LambdaCalculator.hs
+++ haskell/src/LambdaCalculator.hs
@@ -1,16 +1,47 @@
 module LambdaCalculator where
 
-import Data.List
+type Name = String
 
 data Term = Lit Int
-          | Var String Term
-          | App Term   Term
-          | Lam String Term
+          | Var Name
+          | App Term Term
+          | Lam Name Term
           deriving (Eq, Show)
 
-type Env = [(String, Term)]
 
-eval :: Env -> Term -> Term
-eval _ (Lit a)     = Lit a
-eval e (Var n t)   = maybe undefined (\v -> eval e v) (lookup n e)
-eval e (App f@(Lam s t) (Var vs vv)) = eval ((vs, vv):e) f
+------------------------------------------------------------------------------
+type NaiveEnv = [(Name, Term)]
+
+naiveEval :: NaiveEnv -> Term -> Term
+naiveEval = undefined
+
+
+------------------------------------------------------------------------------
+data HOAS = Val Int
+          | Fun (HOAS -> HOAS)
+
+instance Show HOAS where
+  show (Val i) = "Val " ++ show i
+  show (Fun _) = "A function"
+
+instance Eq HOAS where
+  (Val i) == (Val j) = i == j
+  _ == _  = error "Don't know"
+
+type HoasEnv = [(Name, HOAS)]
+
+
+hoasEval :: HoasEnv -> Term -> HOAS
+hoasEval = undefined
+
+
+------------------------------------------------------------------------------
+data ClosureRuntime = Num Int
+                    | Closure Name Term ClosureEnv
+                    deriving (Show, Eq)
+
+
+type ClosureEnv = [(Name, ClosureRuntime)]
+
+closureEval :: ClosureEnv -> Term -> ClosureRuntime
+closureEval = undefined
--- haskell/tests/Tests.hs
+++ haskell/tests/Tests.hs
@@ -9,15 +9,51 @@ import Test.HUnit hiding (Test)
 main :: IO ()
 main = defaultMain [tests]
 
+
 tests :: Test
-tests = testGroup "LambdaCalculator Test Expressions" [
-    testCase "Simple literal eval" simpleLit
-  , testCase "Simple variable assignment" simpleVar
+tests = testGroup "Tests" [
+    testCase "" testNaiveSimpleApp
+  , testCase "" testNaiveCapturing
+  , testCase "" testHOASSimpleApp
+  , testCase "" testCapturingHOAS
+  , testCase "" testClosureSimpleApp
+  , testCase "" testCapturingClosure
   ]
 
-simpleLit :: Assertion
-simpleLit = assertEqual "Simple type lit" (Lit 2) (eval [] (Lit 2))
 
-simpleVar :: Assertion
-simpleVar = assertEqual "Simple variable"
-            (Var "x" (Lit 10)) (eval [] $ Var "x" (Lit 10))
+simpleLit = Lit 1
+
+id_ :: Term
+id_ =  Lam "x" (Var "x")
+
+simpleApp = App id_ (Lit 1)
+
+const_ :: Term
+const_ = Lam "x" (Lam "y" (Var "x"))
+
+
+capturing = App (App const_ (Var "y")) (Lit 10)
+
+testNaiveCapturing = assertEqual "Test failed"
+                     (naiveEval [("y",Lit 42)] capturing)
+                     (Lit 42)
+
+testNaiveSimpleApp = assertEqual ""
+                     (naiveEval [] simpleApp)
+                     (Lit 1)
+
+testHOASSimpleApp = assertEqual ""
+                    (hoasEval [] simpleApp)
+                    (Val 1)
+
+testClosureSimpleApp = assertEqual ""
+                       (closureEval [] simpleApp)
+                       (Num 1)
+
+testCapturingHOAS = assertEqual ""
+                    (hoasEval [] capturing)
+                    (Val 42)
+
+testCapturingClosure = assertEqual ""
+                       (closureEval [] capturing)
+                       (Num 42)
